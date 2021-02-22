089ad125f1137ff12f5769e8c61477974fe52ba5
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-22 20:53:29 +0530

    Big O Rule 1: Worst Case

---
+++ 001-big-o/005.js
@@ -0,0 +1,32 @@
+/**
+ * Big O Rule 1: Worst Case
+ */
+
+{
+  const nemo = ["nemo"];
+  const everyone = [
+    "dory",
+    "bruce",
+    "marlin",
+    "nemo",
+    "gill",
+    "bloat",
+    "nigel",
+    "squirt",
+    "darla",
+    "hank",
+  ];
+  const large = new Array(1000).fill("nemo");
+
+  function findNemo(array) {
+    for (let index = 0; index < array.length; index++) {
+      const element = array[index];
+      if (element === "nemo") {
+        console.log("Found Nemo");
+        break;
+      }
+    }
+  }
+
+  findNemo(large);
+}
