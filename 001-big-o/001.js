02135b9ed2fe98f781ca6233be7c4f73f392ea53
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-22 20:14:16 +0530

    Big O and Scalability

--- 001-big-o/001.js
+++ 001-big-o/001.js
@@ -1,1 +1,35 @@
-console.log("Big O");
+/**
+ * Big O and Scalability
+ */
+
+const { performance } = require("perf_hooks");
+
+const nemo = ["nemo"];
+const everyone = [
+  "dory",
+  "bruce",
+  "marlin",
+  "nemo",
+  "gill",
+  "bloat",
+  "nigel",
+  "squirt",
+  "darla",
+  "hank",
+];
+const large = new Array(1000).fill("nemo");
+
+function findNemo(array) {
+  const timeStart = performance.now();
+  for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    if (element === "nemo") {
+      console.log("Found Nemo");
+    }
+  }
+  const timeEnd = performance.now();
+  const timeTaken = timeEnd - timeStart;
+  console.log(`Call to find Nemo took ${timeTaken} milliseconds.`);
+}
+
+findNemo(large);
