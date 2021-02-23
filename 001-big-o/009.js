312aeb56509089172c89964407bef4732302d7bf
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-23 16:20:58 +0530

    O(n^2)

---
+++ 001-big-o/009.js
@@ -0,0 +1,22 @@
+/**
+ * O(n^2)
+ */
+
+{
+  const boxes = ["a", "b", "c", "d", "e"];
+
+  // Log all Pairs of Array
+  function logAllPairsOfArray(array) {
+    for (let i = 0; i < array.length; i++) {
+      for (let j = 0; j < array.length; j++) {
+        const elementI = array[i];
+        const elementJ = array[j];
+        console.log([elementI, elementJ]);
+      }
+    }
+  }
+
+  logAllPairsOfArray(boxes);
+}
+
+// => O(n * n) => O(n^2)
