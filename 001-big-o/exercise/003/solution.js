2b6497938224668be92a5548ed759198c8d59979
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-23 20:52:03 +0530

    Space Complexity of the given function

---
+++ 001-big-o/exercise/003/solution.js
@@ -0,0 +1,15 @@
+/**
+ * What is the Space Complexity of the given function
+ */
+
+{
+  function booooo(n) {
+    for (let i = 0; i < n.length; i++) {
+      console.log("Booooo...");
+    }
+  }
+
+  booooo([1, 2, 3, 4, 5]);
+}
+
+// => Space: O(1)
