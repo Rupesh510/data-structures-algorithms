1b1e6e2331e27ec73aab569056978b19189ea594
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-23 14:46:31 +0530

    Big O Rule 2: Remove Constants

---
+++ 001-big-o/006.js
@@ -0,0 +1,23 @@
+/**
+ * Big O Rule 2: Remove Constants - Part 1
+ */
+
+{
+  function printFirstItemThenFirstHalfThenSayHi100Times(items) {
+    console.log(items[0]);
+
+    const middleIndex = Math.floor(items.length / 2);
+    let index = 0;
+
+    while (index < middleIndex) {
+      console.log(items[index]);
+      index++;
+    }
+
+    for (let i = 0; i < array.length; i++) {
+      console.log("Hi...");
+    }
+  }
+}
+
+// => O(n/2 + 101) => O(n) => Linear Time
---
+++ 001-big-o/007.js
@@ -0,0 +1,17 @@
+/**
+ * Big O Rule 2: Remove Constants - Part 2
+ */
+
+{
+  function compressBoxesTwice(boxes) {
+    boxes.forEach(function (box) {
+      console.log(box);
+    });
+
+    boxes.forEach(function (box) {
+      console.log(box);
+    });
+  }
+}
+
+// => O(2n) => O(n) => Linear Time
