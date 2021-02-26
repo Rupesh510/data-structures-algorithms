5aa4cad9565c48719dffd250e831792b79c9317f
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-27 01:36:52 +0530

    Two Sum

---
+++ 002-data-structures/001-arrays/exercise/leetcode/001/question.js
@@ -0,0 +1,12 @@
+/**
+ * Two Sum
+ * https://leetcode.com/problems/two-sum/
+ */
+
+{
+  function twoSum() {}
+
+  console.log("twoSum([2, 7, 11, 15], 9):", twoSum([2, 7, 11, 15], 9)); // [0, 1]
+  console.log("twoSum([3, 2, 4], 6):", twoSum([3, 2, 4], 6)); // [1, 2]
+  console.log("twoSum([3, 3], 6):", twoSum([3, 3], 6)); // [0, 1]
+}
---
+++ 002-data-structures/001-arrays/exercise/leetcode/001/solution.js
@@ -0,0 +1,26 @@
+/**
+ * Two Sum
+ * https://leetcode.com/problems/two-sum/
+ */
+
+{
+  function twoSum(nums, target) {
+    const map = new Map();
+    for (let i = 0; i < nums.length; i += 1) {
+      const key = target - nums[i];
+      if (map.has(key)) {
+        return [map.get(key), i];
+      }
+      map.set(nums[i], i);
+    }
+  }
+
+  console.log("twoSum([2, 7, 11, 15], 9):", twoSum([2, 7, 11, 15], 9)); // [0, 1]
+  console.log("twoSum([3, 2, 4], 6):", twoSum([3, 2, 4], 6)); // [1, 2]
+  console.log("twoSum([3, 3], 6):", twoSum([3, 3], 6)); // [0, 1]
+}
+
+/**
+ * Runtime: 72 ms, faster than 94.30% of JavaScript online submissions for Two Sum.
+ * Memory Usage: 38.6 MB, less than 85.68% of JavaScript online submissions for Two Sum.
+ */
