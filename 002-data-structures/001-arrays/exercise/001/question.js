57c546a5785c83c75a6f8460fecff4e4d5e3db7d
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-26 23:04:02 +0530

    Exercise: Reverse a String

---
+++ 002-data-structures/001-arrays/exercise/001/question.js
@@ -0,0 +1,13 @@
+/**
+ * Reverse a String
+ * Create a function that reverses a String
+ */
+
+{
+  function reverse() {}
+
+  console.log(
+    `reverse("Hi, My Name is Harry Manchanda"):`,
+    reverse("Hi, My Name is Harry Manchanda"),
+  ); // adnahcnaM yrraH si emaN yM ,iH
+}
---
+++ 002-data-structures/001-arrays/exercise/001/solution.js
@@ -0,0 +1,58 @@
+/**
+ * Reverse a String
+ * Create a function that reverses a String
+ */
+
+{
+  function reverse(str) {
+    if (!str || str.length < 2 || typeof str !== "string") {
+      return str;
+    }
+
+    const backwards = [];
+    const totalItems = str.length - 1;
+
+    for (let i = totalItems; i >= 0; i -= 1) {
+      backwards.push(str[i]);
+    }
+
+    console.log({ backwards });
+
+    return backwards.join("");
+  }
+
+  console.log(
+    `reverse("Hi, My Name is Harry Manchanda"):`,
+    reverse("Hi, My Name is Harry Manchanda"),
+  ); // adnahcnaM yrraH si emaN yM ,iH
+}
+
+{
+  function reverse(str) {
+    if (!str || str.length < 2 || typeof str !== "string") {
+      return str;
+    }
+
+    return str.split("").reverse().join("");
+  }
+
+  console.log(
+    `reverse("Hi, My Name is Harry Manchanda"):`,
+    reverse("Hi, My Name is Harry Manchanda"),
+  ); // adnahcnaM yrraH si emaN yM ,iH
+}
+
+{
+  function reverse(str) {
+    if (!str || str.length < 2 || typeof str !== "string") {
+      return str;
+    }
+
+    return [...str].reverse().join("");
+  }
+
+  console.log(
+    `reverse("Hi, My Name is Harry Manchanda"):`,
+    reverse("Hi, My Name is Harry Manchanda"),
+  ); // adnahcnaM yrraH si emaN yM ,iH
+}
