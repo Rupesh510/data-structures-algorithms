c70db05f21c5324e74ef8a5e96d897706be6f3a4
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-26 15:31:21 +0530

    Scoping

--- 002-data-structures/001-arrays/002.js
+++ 002-data-structures/001-arrays/002.js
@@ -2,21 +2,23 @@
  * Reference Types in JavaScript
  */
 
-const object1 = {
-  value: 10,
-};
+{
+  const object1 = {
+    value: 10,
+  };
 
-const object2 = object1;
+  const object2 = object1;
 
-const object3 = {
-  value: 10,
-};
+  const object3 = {
+    value: 10,
+  };
 
-console.log("object1 === object2", object1 === object2);
-console.log("object1 === object3", object1 === object3);
+  console.log("object1 === object2", object1 === object2);
+  console.log("object1 === object3", object1 === object3);
 
-object1.value = 15;
+  object1.value = 15;
 
-console.log("object1.value", object1.value);
-console.log("object2.value", object2.value);
-console.log("object3.value", object3.value);
+  console.log("object1.value", object1.value);
+  console.log("object2.value", object2.value);
+  console.log("object3.value", object3.value);
+}
