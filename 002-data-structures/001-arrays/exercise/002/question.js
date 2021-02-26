69cabc8552a82b2d3b86ef884b4fb5bf1a7a452b
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-27 00:24:17 +0530

    Merge Sorted Arrays

---
+++ 002-data-structures/001-arrays/exercise/002/question.js
@@ -0,0 +1,13 @@
+/**
+ * Merge Sorted Arrays
+ * Create a function that merges sorted arrays
+ */
+
+{
+  function mergeSortedArrays() {}
+
+  console.log(
+    `mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]):`,
+    mergeSortedArrays([0, 3, 4, 31], [4, 6, 30]),
+  ); // [0, 3, 4, 6, 30, 31]
+}
