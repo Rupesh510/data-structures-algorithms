4cac4698a53c14cb94fa0d36434537ea8a303adf
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-27 20:25:19 +0530

    More solutions for Two Sum Problem

--- 101-leetcode/001/solution.js
+++ 101-leetcode/001/solution.js
@@ -6,26 +6,78 @@
 
 {
   /**
-   * Approach: One-pass Hash Table
-   * Time complexity: O(n)
-   * Space complexity: O(n)
-   *
-   * Runtime: 72 ms, faster than 94.30% of
-   * JavaScript online submissions for Two Sum.
-   *
-   * Memory Usage: 38.6 MB, less than 85.68% of
-   * JavaScript online submissions for Two Sum.
+   * Approach: Brute Force
+   * Time complexity: O(n^2)
+   * Space complexity: O(1)
    */
 
   function twoSum(nums, target) {
-    const map = new Map();
-    for (let i = 0; i < nums.length; i += 1) {
-      const key = target - nums[i];
-      if (map.has(key)) {
-        return [map.get(key), i];
+    for (let p1 = 0; p1 < nums.length; p1 += 1) {
+      const numberToFind = target - nums[p1];
+      for (let p2 = p1 + 1; p2 < nums.length; p2 += 1) {
+        if (numberToFind === nums[p2]) {
+          return [p1, p2];
+        }
       }
-      map.set(nums[i], i);
     }
+    return null;
+  }
+
+  console.log("twoSum([2, 7, 11, 15], 9):", twoSum([2, 7, 11, 15], 9)); // [0, 1]
+  console.log("twoSum([3, 2, 4], 6):", twoSum([3, 2, 4], 6)); // [1, 2]
+  console.log("twoSum([3, 3], 6):", twoSum([3, 3], 6)); // [0, 1]
+  console.log("twoSum([1, 3, 7, 9, 2], 11):", twoSum([1, 3, 7, 9, 2], 11)); // [3, 4]
+}
+
+console.log("\n================================\n");
+
+{
+  /**
+   * Approach: Hash Map
+   * Time complexity: O(n)
+   * Space complexity: O(n)
+   */
+
+  function twoSum(nums, target) {
+    const numsMap = {};
+    for (let i = 0; i < nums.length; i += 1) {
+      const currentMapVal = numsMap[nums[i]];
+      if (currentMapVal >= 0) {
+        return [currentMapVal, i];
+      } else {
+        const numberToFind = target - nums[i];
+        numsMap[numberToFind] = i;
+      }
+    }
+    return null;
+  }
+
+  console.log("twoSum([2, 7, 11, 15], 9):", twoSum([2, 7, 11, 15], 9)); // [0, 1]
+  console.log("twoSum([3, 2, 4], 6):", twoSum([3, 2, 4], 6)); // [1, 2]
+  console.log("twoSum([3, 3], 6):", twoSum([3, 3], 6)); // [0, 1]
+  console.log("twoSum([1, 3, 7, 9, 2], 11):", twoSum([1, 3, 7, 9, 2], 11)); // [3, 4]
+}
+
+console.log("\n================================\n");
+
+{
+  /**
+   * Approach: Hash Map with `Map` Global Object
+   * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map
+   * Time complexity: O(n)
+   * Space complexity: O(n)
+   */
+
+  function twoSum(nums, target) {
+    const numsMap = new Map();
+    for (let i = 0; i < nums.length; i += 1) {
+      const numberToFind = target - nums[i];
+      if (numsMap.has(numberToFind)) {
+        return [numsMap.get(numberToFind), i];
+      }
+      numsMap.set(nums[i], i);
+    }
+    return null;
   }
 
   console.log("twoSum([2, 7, 11, 15], 9):", twoSum([2, 7, 11, 15], 9)); // [0, 1]
