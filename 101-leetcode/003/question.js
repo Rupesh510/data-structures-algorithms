6eda631573ec5c2cbe81d5dab165bd7137f80743
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-28 19:13:50 +0530

    Create Question: "Trapping Rain Water" Problem

---
+++ 101-leetcode/003/question.js
@@ -0,0 +1,14 @@
+/**
+ * Trapping Rain Water
+ * https://leetcode.com/problems/trapping-rain-water/
+ */
+
+{
+  function trap(height) {}
+
+  console.log(
+    "trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]):",
+    trap([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]),
+  ); // 6
+  console.log("trap([4, 2, 0, 3, 2, 5]):", trap([4, 2, 0, 3, 2, 5])); // 9
+}
