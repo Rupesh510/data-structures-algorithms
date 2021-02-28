84b104ca9686dfedf65e47dc013b0f2c3ae43c48
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-28 17:01:52 +0530

    Add more test cases

--- 101-leetcode/002/question.js
+++ 101-leetcode/002/question.js
@@ -13,5 +13,9 @@
   console.log("maxArea([1, 1]):", maxArea([1, 1])); // 1
   console.log("maxArea([4, 3, 2, 1, 4]):", maxArea([4, 3, 2, 1, 4])); // 16
   console.log("maxArea([1, 2, 1]):", maxArea([1, 2, 1])); // 2
-  console.log("maxArea([1, 8, 6, 2, 9, 4]):", maxArea([1, 8, 6, 2, 9, 4]));
+  console.log("maxArea([1, 8, 6, 2, 9, 4]):", maxArea([1, 8, 6, 2, 9, 4])); // 24
+  console.log("maxArea([1, 7, 2, 0, 1, 3]):", maxArea([1, 7, 2, 0, 1, 3])); // 12
+  console.log("maxArea([6, 9, 3, 4, 5, 8]):", maxArea([6, 9, 3, 4, 5, 8])); // 32
+  console.log("maxArea([]):", maxArea([])); // 0
+  console.log("maxArea([7]):", maxArea([7])); // 0
 }
