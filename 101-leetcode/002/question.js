75b91052c45ab201f9ed81723f03b26d624015c0
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-28 16:05:31 +0530

    Create Question: Container With Most Water

---
+++ 101-leetcode/002/question.js
@@ -0,0 +1,17 @@
+/**
+ * Container With Most Water
+ * https://leetcode.com/problems/container-with-most-water/
+ */
+
+{
+  function maxArea(height) {}
+
+  console.log(
+    "maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]):",
+    maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]),
+  ); // 49
+  console.log("maxArea([1, 1]):", maxArea([1, 1])); // 1
+  console.log("maxArea([4, 3, 2, 1, 4]):", maxArea([4, 3, 2, 1, 4])); // 16
+  console.log("maxArea([1, 2, 1]):", maxArea([1, 2, 1])); // 2
+  console.log("maxArea([1, 8, 6, 2, 9, 4]):", maxArea([1, 8, 6, 2, 9, 4]));
+}
