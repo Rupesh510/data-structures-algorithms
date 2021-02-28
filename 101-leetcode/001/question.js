c017621533b9ce5b8388de5533be70c15558144e
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-02-28 17:11:13 +0530

    Update question for two sum

--- 101-leetcode/001/question.js
+++ 101-leetcode/001/question.js
@@ -4,7 +4,7 @@
  */
 
 {
-  function twoSum() {}
+  function twoSum(nums, target) {}
 
   console.log("twoSum([2, 7, 11, 15], 9):", twoSum([2, 7, 11, 15], 9)); // [0, 1]
   console.log("twoSum([3, 2, 4], 6):", twoSum([3, 2, 4], 6)); // [1, 2]
