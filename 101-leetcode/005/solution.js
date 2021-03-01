ab5e715d30c34d27ef975b4dd838feecd124163a
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-01 21:50:42 +0530

    Carbon images & content updates
    for Move Zeroes problem

--- 101-leetcode/005/solution.js
+++ 101-leetcode/005/solution.js
@@ -26,5 +26,5 @@
     "moveZeroes([0, 0, 6, 0, 12, 1]):",
     moveZeroes([0, 0, 6, 0, 12, 1]),
   ); // [6, 12, 1, 0, 0, 0]
-  console.log("moveZeroes([3, 0, 0, 12, 0]):", moveZeroes([3, 0, 0, 12, 0])); // [3, 12, 0, 0, 0];
+  console.log("moveZeroes([3, 0, 0, 12, 0]):", moveZeroes([3, 0, 0, 12, 0])); // [3, 12, 0, 0, 0]
 }
