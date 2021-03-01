dafdcaa9df7f3f2bb661aa8135a1483a42376e71
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-01 16:02:50 +0530

    Optimal solution for Maximum Subarray problem

---
+++ 101-leetcode/004/carbon/001.js
@@ -0,0 +1,16 @@
+/**
+ * Approach: Optimal
+ * Time complexity: O(n)
+ */
+
+function maxSubArray(nums) {
+  let maxCurrent = nums[0];
+  let maxGlobal = nums[0];
+  for (let i = 1; i < nums.length; i += 1) {
+    maxCurrent = Math.max(nums[i], maxCurrent + nums[i]);
+    if (maxCurrent > maxGlobal) {
+      maxGlobal = maxCurrent;
+    }
+  }
+  return maxGlobal;
+}
--- 101-leetcode/004/solution.js
+++ 101-leetcode/004/solution.js
@@ -1,10 +1,26 @@
 /**
  * Maximum Subarray
  * https://leetcode.com/problems/maximum-subarray/
+ * Solution Ref: https://youtu.be/gwUGDXO5gHU
  */
 
 {
-  function maxSubArray(nums) {}
+  /**
+   * Approach: Optimal
+   * Time complexity: O(n)
+   */
+
+  function maxSubArray(nums) {
+    let maxCurrent = nums[0];
+    let maxGlobal = nums[0];
+    for (let i = 1; i < nums.length; i += 1) {
+      maxCurrent = Math.max(nums[i], maxCurrent + nums[i]);
+      if (maxCurrent > maxGlobal) {
+        maxGlobal = maxCurrent;
+      }
+    }
+    return maxGlobal;
+  }
 
   console.log(
     "maxSubArray([-2, 1, -3, 4, -1, 2, 1, -5, 4]):",
