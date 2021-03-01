e39bb1d7e5b09476c09216417df77509bda0b5e6
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-01 21:51:02 +0530

    Create Question: "Rotate Array" Problem

---
+++ 101-leetcode/006/question.js
@@ -0,0 +1,14 @@
+/**
+ * Rotate Array
+ * https://leetcode.com/problems/rotate-array/
+ */
+
+{
+  function rotate(nums, k) {}
+
+  console.log(
+    "rotate([1, 2, 3, 4, 5, 6, 7], 3):",
+    rotate([1, 2, 3, 4, 5, 6, 7], 3),
+  ); // [5, 6, 7, 1, 2, 3, 4]
+  console.log("rotate([-1, -100, 3, 99], 2):", rotate([-1, -100, 3, 99], 2)); // [3, 99, -1, -100]
+}
