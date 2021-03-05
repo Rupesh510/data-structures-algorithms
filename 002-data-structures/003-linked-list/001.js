e61723826639a6e83c8a35e64eed0cccde8fddfc
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-05 15:11:33 +0530

    append() method for linked list

--- 002-data-structures/003-linked-list/001.js
+++ 002-data-structures/003-linked-list/001.js
@@ -11,7 +11,26 @@ class LinkedList {
     this.tail = this.head;
     this.length = 1;
   }
+
+  append(value) {
+    const newNode = {
+      value,
+      next: null,
+    };
+    this.tail.next = newNode;
+    this.tail = newNode;
+    this.length += 1;
+    return this;
+  }
 }
 
 const myLinkedList = new LinkedList(10);
-console.log("myLinkedList:", myLinkedList);
+console.log("myLinkedList:", JSON.stringify(myLinkedList, null, 2));
+console.log(
+  "myLinkedList.append(5):",
+  JSON.stringify(myLinkedList.append(5), null, 2),
+);
+console.log(
+  "myLinkedList.append(16):",
+  JSON.stringify(myLinkedList.append(16), null, 2),
+);
