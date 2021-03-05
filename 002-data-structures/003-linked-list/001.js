13abe6e1abdb683a99efe86f8d41fb50020584b2
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-05 15:40:18 +0530

    printList

--- 002-data-structures/003-linked-list/001.js
+++ 002-data-structures/003-linked-list/001.js
@@ -19,6 +19,16 @@ class LinkedList {
     this.length = 1;
   }
 
+  printList() {
+    const array = [];
+    let currentNode = this.head;
+    while (currentNode !== null) {
+      array.push(currentNode.value);
+      currentNode = currentNode.next;
+    }
+    return array;
+  }
+
   append(value) {
     const newNode = new Node(value);
     this.tail.next = newNode;
@@ -34,19 +44,61 @@ class LinkedList {
     this.length += 1;
     return this;
   }
+
+  // insert(index, value) {}
 }
 
 const myLinkedList = new LinkedList(10);
-console.log("myLinkedList:", JSON.stringify(myLinkedList, null, 2));
+console.log(
+  "myLinkedList:",
+  JSON.stringify(
+    {
+      data: myLinkedList,
+      list: myLinkedList.printList(),
+    },
+    null,
+    2,
+  ),
+);
+
+console.log("\n================================\n");
+
 console.log(
   "myLinkedList.append(5):",
-  JSON.stringify(myLinkedList.append(5), null, 2),
+  JSON.stringify(
+    {
+      data: myLinkedList.append(5),
+      list: myLinkedList.printList(),
+    },
+    null,
+    2,
+  ),
 );
+
+console.log("\n================================\n");
+
 console.log(
   "myLinkedList.append(16):",
-  JSON.stringify(myLinkedList.append(16), null, 2),
+  JSON.stringify(
+    {
+      data: myLinkedList.append(16),
+      list: myLinkedList.printList(),
+    },
+    null,
+    2,
+  ),
 );
+
+console.log("\n================================\n");
+
 console.log(
   "myLinkedList.prepend(1):",
-  JSON.stringify(myLinkedList.prepend(1), null, 2),
+  JSON.stringify(
+    {
+      data: myLinkedList.prepend(1),
+      list: myLinkedList.printList(),
+    },
+    null,
+    2,
+  ),
 );
