5d6cd83b6612f032c77db28de3dbe905447500f1
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-05 14:16:36 +0530

    Starting work on First Linked List

---
+++ 002-data-structures/003-linked-list/001.js
@@ -0,0 +1,17 @@
+/**
+ * Linked List
+ */
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value,
+      next: null,
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+}
+
+const myLinkedList = new LinkedList(10);
+console.log("myLinkedList:", myLinkedList);
