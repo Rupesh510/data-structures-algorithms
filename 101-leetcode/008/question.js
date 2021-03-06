8507179cd1c3efad4c4f0aea769573b35f17db5e
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-06 17:50:51 +0530

    Better Logging

--- 101-leetcode/008/question.js
+++ 101-leetcode/008/question.js
@@ -6,7 +6,16 @@
 {
   function containsNearbyDuplicate(nums, k) {}
 
-  console.log(containsNearbyDuplicate([1, 2, 3, 1], 3)); // true
-  console.log(containsNearbyDuplicate([1, 0, 1, 1], 1)); // true
-  console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2)); // false
+  console.log(
+    "containsNearbyDuplicate([1, 2, 3, 1], 3):",
+    containsNearbyDuplicate([1, 2, 3, 1], 3),
+  ); // true
+  console.log(
+    "containsNearbyDuplicate([1, 0, 1, 1], 1):",
+    containsNearbyDuplicate([1, 0, 1, 1], 1),
+  ); // true
+  console.log(
+    "containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2):",
+    containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2),
+  ); // false
 }
