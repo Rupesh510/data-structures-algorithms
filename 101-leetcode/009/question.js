c6906b70221fe14acf932d5fc51f421a62aab159
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-06 20:01:55 +0530

    Add more test log cases in Backspace String Compare

--- 101-leetcode/009/question.js
+++ 101-leetcode/009/question.js
@@ -19,4 +19,24 @@
     backspaceCompare("a##c", "#a#c"),
   ); // true
   console.log(`backspaceCompare("a#c", "b"):`, backspaceCompare("a#c", "b")); // false
+  console.log(
+    `backspaceCompare("ab#z", "az#z"):`,
+    backspaceCompare("ab#z", "az#z"),
+  ); // true
+  console.log(
+    `backspaceCompare("abc#d", "acc#c"):`,
+    backspaceCompare("abc#d", "acc#c"),
+  ); // false
+  console.log(
+    `backspaceCompare("x#y#z#", "a#"):`,
+    backspaceCompare("x#y#z#", "a#"),
+  ); // true
+  console.log(
+    `backspaceCompare("a###b", "b"):`,
+    backspaceCompare("a###b", "b"),
+  ); // true
+  console.log(
+    `backspaceCompare("Ab#z", "ab#z"):`,
+    backspaceCompare("Ab#z", "ab#z"),
+  ); // false
 }
