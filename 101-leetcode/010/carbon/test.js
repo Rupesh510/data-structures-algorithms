0bc6e0c0b3147497b19b11a30f7ae8ae56da0e3a
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-03-13 17:30:58 +0530

    Brute Force Solution for
    Longest Substring Without Repeating Characters

---
+++ 101-leetcode/010/carbon/test.js
@@ -0,0 +1,6 @@
+lengthOfLongestSubstring("abccabb"); // 3
+lengthOfLongestSubstring("abcbdca"); // 4
+lengthOfLongestSubstring("abcabcbb"); // 3
+lengthOfLongestSubstring("bbbbb"); // 1
+lengthOfLongestSubstring("pwwkew"); // 3
+lengthOfLongestSubstring(""); // 0
--- 101-leetcode/010/solution.js
+++ 101-leetcode/010/solution.js
@@ -5,7 +5,31 @@
  */
 
 {
-  function lengthOfLongestSubstring(s) {}
+  /**
+   * Approach: Brute Force
+   * Time complexity: O(n^2)
+   * Space complexity: O(n)
+   */
+
+  function lengthOfLongestSubstring(s) {
+    if (s.length <= 1) return s.length;
+    let longest = 0;
+    for (let left = 0; left < s.length; left += 1) {
+      let seenChars = {},
+        currentLength = 0;
+      for (let right = left; right < s.length; right += 1) {
+        const currentChar = s[right];
+        if (!seenChars[currentChar]) {
+          currentLength += 1;
+          seenChars[currentChar] = true;
+          longest = Math.max(longest, currentLength);
+        } else {
+          break;
+        }
+      }
+    }
+    return longest;
+  }
 
   console.log(
     `lengthOfLongestSubstring("abccabb"):`,
