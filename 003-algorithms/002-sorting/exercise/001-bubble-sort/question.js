b2364e45022b4110bbc2327a700bfbedcb62f471
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-04-14 15:41:16 +0530

    Bubble Sort

---
+++ 003-algorithms/002-sorting/exercise/001-bubble-sort/question.js
@@ -0,0 +1,7 @@
+/**
+ * Bubble Sort
+ */
+
+function bubbleSort(array) {}
+
+console.log(bubbleSort([99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]));
---
+++ 003-algorithms/002-sorting/exercise/001-bubble-sort/solution.js
@@ -0,0 +1,18 @@
+/**
+ * Bubble Sort
+ */
+
+function bubbleSort(array) {
+  const length = array.length;
+  for (let i = 0; i < length; i += 1) {
+    for (let j = 0; j < length; j += 1) {
+      if (array[j] > array[j + 1]) {
+        [array[j], array[j + 1]] = [array[j + 1], array[j]];
+      }
+    }
+  }
+
+  return array;
+}
+
+console.log(bubbleSort([99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]));
