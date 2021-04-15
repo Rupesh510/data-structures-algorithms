b1fa3ac6002e338aa8585095fc5e8e98b7a1d50c
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-04-15 13:25:45 +0530

    Create Question: Kth Largest Element in an Array

---
+++ 101-leetcode/021/question.js
@@ -0,0 +1,18 @@
+/**
+ * Kth Largest Element in an Array
+ * https://leetcode.com/problems/kth-largest-element-in-an-array/
+ */
+
+{
+  function findKthLargest(nums, k) {}
+
+  console.log(
+    `findKthLargest([3, 2, 1, 5, 6, 4], 2):`,
+    findKthLargest([3, 2, 1, 5, 6, 4], 2),
+  ); // 5
+
+  console.log(
+    `findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4):`,
+    findKthLargest([3, 2, 3, 1, 2, 4, 5, 5, 6], 4),
+  ); // 4
+}
