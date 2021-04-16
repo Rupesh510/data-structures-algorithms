3ed672a68737c20f2f41ac33f9ee767170a2e2d8
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-04-16 14:24:53 +0530

    Lookup: Binary Search Tree

--- 002-data-structures/005-trees/exercise/001-binary-search-tree/question.js
+++ 002-data-structures/005-trees/exercise/001-binary-search-tree/question.js
@@ -22,7 +22,19 @@ class BinarySearchTree {
   remove(value) {}
 }
 
+const traverse = (node) => ({
+  value: node.value,
+  left: node.left === null ? null : traverse(node.left),
+  right: node.right === null ? null : traverse(node.right),
+});
+
 const tree = new BinarySearchTree();
+
+console.log(
+  `tree.lookup(12):`,
+  tree.lookup(12) ? tree.lookup(12).value : tree.lookup(12),
+);
+
 tree.insert(9);
 tree.insert(4);
 tree.insert(6);
@@ -31,11 +43,6 @@ tree.insert(170);
 tree.insert(15);
 tree.insert(1);
 
-const traverse = (node) => ({
-  value: node.value,
-  left: node.left === null ? null : traverse(node.left),
-  right: node.right === null ? null : traverse(node.right),
-});
 console.log(
   `traverse(tree.root):`,
   JSON.stringify(traverse(tree.root), null, 2),
@@ -45,3 +52,13 @@ console.log(
  *   4     20
  * 1  6  15  170
  */
+
+console.log(
+  `tree.lookup(15):`,
+  tree.lookup(15) ? tree.lookup(15).value : tree.lookup(15),
+); // 15
+
+console.log(
+  `tree.lookup(14):`,
+  tree.lookup(14) ? tree.lookup(14).value : tree.lookup(14),
+); // null
--- 002-data-structures/005-trees/exercise/001-binary-search-tree/solution.js
+++ 002-data-structures/005-trees/exercise/001-binary-search-tree/solution.js
@@ -46,12 +46,39 @@ class BinarySearchTree {
     }
   }
 
-  lookup(value) {}
+  lookup(value) {
+    if (!this.root) return false;
+    let currentNode = this.root;
+
+    while (currentNode) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else if (currentNode.value === value) {
+        return currentNode;
+      }
+    }
+
+    return null;
+  }
 
   remove(value) {}
 }
 
+const traverse = (node) => ({
+  value: node.value,
+  left: node.left === null ? null : traverse(node.left),
+  right: node.right === null ? null : traverse(node.right),
+});
+
 const tree = new BinarySearchTree();
+
+console.log(
+  `tree.lookup(12):`,
+  tree.lookup(12) ? tree.lookup(12).value : tree.lookup(12),
+);
+
 tree.insert(9);
 tree.insert(4);
 tree.insert(6);
@@ -60,11 +87,6 @@ tree.insert(170);
 tree.insert(15);
 tree.insert(1);
 
-const traverse = (node) => ({
-  value: node.value,
-  left: node.left === null ? null : traverse(node.left),
-  right: node.right === null ? null : traverse(node.right),
-});
 console.log(
   `traverse(tree.root):`,
   JSON.stringify(traverse(tree.root), null, 2),
@@ -74,3 +96,13 @@ console.log(
  *   4     20
  * 1  6  15  170
  */
+
+console.log(
+  `tree.lookup(15):`,
+  tree.lookup(15) ? tree.lookup(15).value : tree.lookup(15),
+); // 15
+
+console.log(
+  `tree.lookup(14):`,
+  tree.lookup(14) ? tree.lookup(14).value : tree.lookup(14),
+); // null
