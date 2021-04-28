20d2e9d1dfa7688bae512bf5f5a49b63558089ac
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-04-28 20:58:35 +0530

    Fix leetcode link

--- 101-leetcode/024/solution.js
+++ 101-leetcode/024/solution.js
@@ -38,6 +38,6 @@
 
   console.log(`
     Please run the solution code with tests cases at Leetcode website instead,
-    https://leetcode.com/problems/maximum-depth-of-binary-tree/
+    https://leetcode.com/problems/binary-tree-level-order-traversal/
   `);
 }
