14c2d66c57d09a89fb9b462e7739ad8ea5f33d9f
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-04-28 20:19:12 +0530

    Solution & Test Cases & Carbon Images
    for Binary Tree Level Order Traversal

---
+++ 101-leetcode/024/carbon/001.js
@@ -0,0 +1,34 @@
+function levelOrder(root) {
+  let result = [];
+
+  if (root === null) {
+    return result;
+  }
+
+  let queue = [];
+  queue.push(root);
+
+  while (queue.length > 0) {
+    let row = [];
+    let rowSize = queue.length;
+
+    while (rowSize > 0) {
+      let currentNode = queue.shift();
+
+      if (currentNode.left !== null) {
+        queue.push(currentNode.left);
+      }
+
+      if (currentNode.right !== null) {
+        queue.push(currentNode.right);
+      }
+
+      row.push(currentNode.val);
+      rowSize -= 1;
+    }
+
+    result.push(row);
+  }
+
+  return result;
+}
--- 101-leetcode/024/solution.js
+++ 101-leetcode/024/solution.js
@@ -4,7 +4,40 @@
  */
 
 {
-  function levelOrder(root) {}
+  function levelOrder(root) {
+    let result = [];
+
+    if (root === null) {
+      return result;
+    }
+
+    let queue = [];
+    queue.push(root);
+
+    while (queue.length > 0) {
+      let row = [];
+      let rowSize = queue.length;
+
+      while (rowSize > 0) {
+        let currentNode = queue.shift();
+
+        if (currentNode.left !== null) {
+          queue.push(currentNode.left);
+        }
+
+        if (currentNode.right !== null) {
+          queue.push(currentNode.right);
+        }
+
+        row.push(currentNode.val);
+        rowSize -= 1;
+      }
+
+      result.push(row);
+    }
+
+    return result;
+  }
 
   console.log(`
     Please run the solution code with tests cases at Leetcode website instead,
