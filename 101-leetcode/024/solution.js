75656ed6337f2d6a2b9867568f28f7dbd1e9dcbb
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-04-28 17:47:25 +0530

    Create Question "Binary Tree Level Order Traversal"

---
+++ 101-leetcode/024/solution.js
@@ -0,0 +1,13 @@
+/**
+ * Binary Tree Level Order Traversal
+ * https://leetcode.com/problems/binary-tree-level-order-traversal/
+ */
+
+{
+  function levelOrder(root) {}
+
+  console.log(`
+    Please run the solution code with tests cases at Leetcode website instead,
+    https://leetcode.com/problems/maximum-depth-of-binary-tree/
+  `);
+}
