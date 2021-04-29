63c58bd6da6b9f7d62a86c14b07c74518d3827ec
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-04-29 11:23:44 +0530

    Create Exercise "Binary Tree Right Side View"

---
+++ 101-leetcode/025/question.js
@@ -0,0 +1,13 @@
+/**
+ * Binary Tree Right Side View
+ * https://leetcode.com/problems/binary-tree-right-side-view/
+ */
+
+{
+  function rightSideView(root) {}
+
+  console.log(`
+    Please run the solution code with tests cases at Leetcode website instead,
+    https://leetcode.com/problems/maximum-depth-of-binary-tree/
+  `);
+}
---
+++ 101-leetcode/025/solution.js
@@ -0,0 +1,32 @@
+/**
+ * Binary Tree Right Side View
+ * https://leetcode.com/problems/binary-tree-right-side-view/
+ */
+
+{
+  /**
+   * Approach: Breadth First Search
+   */
+
+  function rightSideView(root) {}
+
+  console.log(`
+    --- Approach: Breadth First Search ---
+    Please run the solution code with tests cases at Leetcode website instead,
+    https://leetcode.com/problems/binary-tree-right-side-view/
+  `);
+}
+
+{
+  /**
+   * Approach: Depth First Search
+   */
+
+  function rightSideView(root) {}
+
+  console.log(`
+    --- Approach: Depth First Search ---
+    Please run the solution code with tests cases at Leetcode website instead,
+    https://leetcode.com/problems/binary-tree-right-side-view/
+  `);
+}
