35784a7752e761795a51bce4e8a7189f6aecc175
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-04-29 14:34:21 +0530

    Create Exercise - Count Complete Tree Nodes

---
+++ 101-leetcode/026/question.js
@@ -0,0 +1,13 @@
+/**
+ * Count Complete Tree Nodes
+ * https://leetcode.com/problems/count-complete-tree-nodes/
+ */
+
+{
+  function countNodes(root) {}
+
+  console.log(`
+    Please run the solution code with tests cases at Leetcode website instead,
+    https://leetcode.com/problems/count-complete-tree-nodes/
+  `);
+}
