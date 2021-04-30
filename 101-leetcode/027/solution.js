2eed0f1a60e5e5cba130041a9cf2227de536db01
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-04-30 17:49:05 +0530

    Create Exercise Validate Binary Search Tree

---
+++ 101-leetcode/027/solution.js
@@ -0,0 +1,13 @@
+/**
+ * Validate Binary Search Tree
+ * https://leetcode.com/problems/validate-binary-search-tree/
+ */
+
+{
+  function isValidBST(root) {}
+
+  console.log(`
+    Please run the solution code with tests cases at Leetcode website instead,
+    https://leetcode.com/problems/validate-binary-search-tree/
+  `);
+}
