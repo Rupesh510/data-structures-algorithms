72c6159a8b00616f3173cbad3cb1466d1ff14c52
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-01 23:44:02 +0530

    Carbon Images for 2-D Arrays (Matrix) Traversal

---
+++ 002-data-structures/008-2d-arrays/carbon/002.js
@@ -0,0 +1,56 @@
+/**
+ * 2-D Arrays (Matrix) Traversal
+ * Approach: Breadth First Search
+ */
+
+const directions = [
+  [-1, 0], // up
+  [0, 1], // right
+  [1, 0], // down
+  [0, -1], // left
+];
+
+function traversalBreadthFirstSearch(matrix) {
+  const seen = new Array(matrix.length)
+    .fill(0)
+    .map(() => new Array(matrix[0].length).fill(false));
+
+  const values = [];
+  const queue = [[0, 0]];
+
+  while (queue.length) {
+    const currentPos = queue.shift();
+    const row = currentPos[0];
+    const col = currentPos[1];
+
+    if (
+      row < 0 ||
+      row >= matrix.length ||
+      col < 0 ||
+      col >= matrix[0].length ||
+      seen[row][col]
+    ) {
+      continue;
+    }
+
+    seen[row][col] = true;
+    values.push(matrix[row][col]);
+
+    for (let i = 0; i < directions.length; i += 1) {
+      const currentDir = directions[i];
+      queue.push([row + currentDir[0], col + currentDir[1]]);
+    }
+  }
+
+  return values;
+}
+
+traversalBreadthFirstSearch([
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
+  [11, 12, 13, 14, 15],
+  [16, 17, 18, 19, 20],
+]); /*
+  [1, 2, 6, 3, 7, 11, 4, 8, 12, 16, 5, 
+   9, 13, 17, 10, 14, 18, 15, 19, 20]
+ */
