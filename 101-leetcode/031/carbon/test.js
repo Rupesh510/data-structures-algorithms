4b703a0dccc5a83d21eb1a930cff6b276ac564a6
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-07 00:44:14 +0530

    Naive Breadth First Search for Course Schedule

---
+++ 101-leetcode/031/carbon/test.js
@@ -0,0 +1,5 @@
+canFinish(2, [[1, 0]]); // true
+canFinish(2, [
+  [1, 0],
+  [0, 1],
+]); // false
--- 101-leetcode/031/solution.js
+++ 101-leetcode/031/solution.js
@@ -4,7 +4,45 @@
  */
 
 {
-  function canFinish(numCourses, prerequisites) {}
+  /**
+   * Approach: Naive Breadth First Search
+   */
+
+  function canFinish(numCourses, prerequisites) {
+    const adjList = new Array(numCourses).fill(0).map(() => []);
+
+    for (let i = 0; i < prerequisites.length; i += 1) {
+      const pair = prerequisites[i];
+      adjList[pair[1]].push(pair[0]);
+    }
+
+    for (let v = 0; v < numCourses; v += 1) {
+      const queue = [];
+      const seen = {};
+
+      for (let i = 0; i < adjList[v].length; i += 1) {
+        queue.push(adjList[v][i]);
+      }
+
+      while (queue.length) {
+        const current = queue.shift();
+        seen[current] = true;
+
+        if (current === v) return false;
+
+        const adjacent = adjList[current];
+
+        for (let i = 0; i < adjacent.length; i += 1) {
+          const next = adjacent[i];
+          if (!seen[next]) {
+            queue.push(next);
+          }
+        }
+      }
+    }
+
+    return true;
+  }
 
   console.log(`canFinish(2, [[1, 0]]):`, canFinish(2, [[1, 0]])); // true
 
