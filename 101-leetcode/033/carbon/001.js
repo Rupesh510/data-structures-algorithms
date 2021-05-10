52e4d94c7d091ff5b515bf2950e596cf232c7510
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-10 17:40:44 +0530

    More work

---
+++ 101-leetcode/033/carbon/001.js
@@ -0,0 +1,22 @@
+/**
+ * Approach: Top down recursive memoized
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+
+function minCostClimbingStairs(cost) {
+  const n = cost.length;
+  const dp = [];
+  return Math.min(minCost(n - 1, cost, dp), minCost(n - 2, cost, dp));
+}
+
+function minCost(i, cost, dp) {
+  if (i < 0) return 0;
+  if (i === 0 || i === 1) return cost[i];
+  if (dp[i] !== undefined) return dp[i];
+
+  dp[i] =
+    cost[i] + Math.min(minCost(i - 1, cost, dp), minCost(i - 2, cost, dp));
+
+  return dp[i];
+}
