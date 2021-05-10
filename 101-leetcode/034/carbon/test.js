561a638a31bca584670f6a2ee69f8fcfc996354b
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-10 22:53:43 +0530

    Create exercise "Knight Probability in Chessboard"

---
+++ 101-leetcode/034/carbon/test.js
@@ -0,0 +1,2 @@
+knightProbability(3, 2, 0, 0); // 0.06250
+knightProbability(1, 0, 0, 0); // 1.00000
---
+++ 101-leetcode/034/solution.js
@@ -0,0 +1,18 @@
+/**
+ * Knight Probability in Chessboard
+ * https://leetcode.com/problems/knight-probability-in-chessboard/
+ */
+
+{
+  function knightProbability(n, k, row, column) {}
+
+  console.log(
+    `knightProbability(3, 2, 0, 0):`,
+    parseFloat(knightProbability(3, 2, 0, 0)).toFixed(5),
+  ); // 0.06250
+
+  console.log(
+    `knightProbability(1, 0, 0, 0):`,
+    parseFloat(knightProbability(1, 0, 0, 0)).toFixed(5),
+  ); // 1.00000
+}
