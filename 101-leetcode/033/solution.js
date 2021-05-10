d5fed46fb94cd0858c310890a2f97b0abf7fdb7e
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-10 18:16:04 +0530

    Top down recursive (no memo) solution for
    Min Cost Climbing Stairs exercise
    
    ---
    
    Note: Earlier I didn't want to add this inefficent solution but
    I guess it's good for notes if I add this one too!

--- 101-leetcode/033/solution.js
+++ 101-leetcode/033/solution.js
@@ -5,6 +5,32 @@
 
 {
   /**
+   * Approach: Top down recursive
+   * Time Complexity: O(2^n)
+   * Space Complexity: O(n)
+   */
+
+  function minCostClimbingStairs(cost) {
+    const n = cost.length;
+    return Math.min(minCost(n - 1, cost), minCost(n - 2, cost));
+  }
+
+  function minCost(i, cost) {
+    if (i < 0) return 0;
+    if (i === 0 || i === 1) return cost[i];
+
+    return cost[i] + Math.min(minCost(i - 1, cost), minCost(i - 2, cost));
+  }
+
+  console.log(minCostClimbingStairs([10, 15, 20])); // 15
+
+  console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])); // 6
+}
+
+console.log("\n================================\n");
+
+{
+  /**
    * Approach: Top down recursive memoized
    * Time Complexity: O(n)
    * Space Complexity: O(n)
