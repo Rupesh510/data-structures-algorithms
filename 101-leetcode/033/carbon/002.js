a08b561df015fe560ee4160bebb97df798f7e1a1
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-10 17:47:05 +0530

    Bottom up iterative memoized solution for
    Min Cost Climbing Stairs exercise

---
+++ 101-leetcode/033/carbon/002.js
@@ -0,0 +1,24 @@
+/**
+ * Approach: Bottom up iterative memoized
+ * Time Complexity: O(n)
+ * Space Complexity: O(n)
+ */
+
+function minCostClimbingStairs(cost) {
+  const n = cost.length;
+
+  if (n === 0) return 0;
+  if (n === 1) return cost[0];
+
+  const dp = [];
+
+  for (let i = 0; i < n; i += 1) {
+    if (i < 2) {
+      dp[i] = cost[i];
+    } else {
+      dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+    }
+  }
+
+  return Math.min(dp[n - 1], dp[n - 2]);
+}
--- 101-leetcode/033/solution.js
+++ 101-leetcode/033/solution.js
@@ -31,3 +31,36 @@
 
   console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])); // 6
 }
+
+console.log("\n================================\n");
+
+{
+  /**
+   * Approach: Bottom up iterative memoized
+   * Time Complexity: O(n)
+   * Space Complexity: O(n)
+   */
+
+  function minCostClimbingStairs(cost) {
+    const n = cost.length;
+
+    if (n === 0) return 0;
+    if (n === 1) return cost[0];
+
+    const dp = [];
+
+    for (let i = 0; i < n; i += 1) {
+      if (i < 2) {
+        dp[i] = cost[i];
+      } else {
+        dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
+      }
+    }
+
+    return Math.min(dp[n - 1], dp[n - 2]);
+  }
+
+  console.log(minCostClimbingStairs([10, 15, 20])); // 15
+
+  console.log(minCostClimbingStairs([1, 100, 1, 1, 1, 100, 1, 1, 100, 1])); // 6
+}
