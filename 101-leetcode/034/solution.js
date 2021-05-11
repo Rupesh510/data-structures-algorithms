987db9172c7e275a9c64e2a992fb01c53d5d5474
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-11 20:04:54 +0530

    Approach: Step 3, Bottom up iterative memoized
    solution for "Knight Probability in Chessboard" exercise

--- 101-leetcode/034/solution.js
+++ 101-leetcode/034/solution.js
@@ -92,3 +92,60 @@ console.log("\n================================\n");
     parseFloat(knightProbability(1, 0, 0, 0)).toFixed(5),
   ); // 1.00000
 }
+
+console.log("\n================================\n");
+
+{
+  /**
+   * Approach: Step 3, Bottom up iterative memoized
+   * Time Complexity: O(n^2 x k)
+   * Space Complexity: O(n^2 x k)
+   */
+
+  function knightProbability(n, k, row, column) {
+    const dp = new Array(k + 1).fill(0).map(() => {
+      return new Array(n).fill(0).map(() => {
+        return new Array(n).fill(0);
+      });
+    });
+
+    dp[0][row][column] = 1;
+
+    for (let step = 1; step <= k; step += 1) {
+      for (let row = 0; row < n; row += 1) {
+        for (let col = 0; col < n; col += 1) {
+          for (let i = 0; i < DIRECTIONS.length; i += 1) {
+            const dir = DIRECTIONS[i];
+            const prevRow = row + dir[0];
+            const prevCol = col + dir[1];
+
+            if (prevRow >= 0 && prevRow < n && prevCol >= 0 && prevCol < n) {
+              dp[step][row][col] =
+                dp[step][row][col] + dp[step - 1][prevRow][prevCol] / 8;
+            }
+          }
+        }
+      }
+    }
+
+    let res = 0;
+
+    for (let i = 0; i < n; i += 1) {
+      for (let j = 0; j < n; j += 1) {
+        res += dp[k][i][j];
+      }
+    }
+
+    return res;
+  }
+
+  console.log(
+    `knightProbability(3, 2, 0, 0):`,
+    parseFloat(knightProbability(3, 2, 0, 0)).toFixed(5),
+  ); // 0.06250
+
+  console.log(
+    `knightProbability(1, 0, 0, 0):`,
+    parseFloat(knightProbability(1, 0, 0, 0)).toFixed(5),
+  ); // 1.00000
+}
