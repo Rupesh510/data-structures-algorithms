becd829826dbffb5c922e61a0b6b85579871b4d9
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-17 15:38:25 +0530

    Create exercise "Climbing Stairs"

---
+++ 101-leetcode/037/solution.js
@@ -0,0 +1,25 @@
+/**
+ * Climbing Stairs
+ * https://leetcode.com/problems/climbing-stairs/
+ */
+
+{
+  function climbStairs(n) {}
+
+  console.log(`climbStairs(2):`, climbStairs(2)); // 2
+  /**
+   * Answer: 2
+   * Explanation: There are two ways to climb to the top.
+   * 1. 1 step + 1 step
+   * 2. 2 steps
+   */
+
+  console.log(`climbStairs(3):`, climbStairs(3)); // 3
+  /**
+   * Answer: 3
+   * Explanation: There are three ways to climb to the top.
+   * 1. 1 step + 1 step + 1 step
+   * 2. 1 step + 2 steps
+   * 3. 2 steps + 1 step
+   */
+}
