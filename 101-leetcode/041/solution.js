4d84038ce4111f62ac4621743b94d294b054d7a4
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-18 21:51:36 +0530

    Create Exercise "Plus One"

---
+++ 101-leetcode/041/solution.js
@@ -0,0 +1,12 @@
+/**
+ * Plus One
+ * https://leetcode.com/problems/plus-one/
+ */
+
+{
+  function plusOne(digits) {}
+
+  console.log(`plusOne([1, 2, 3]):`, plusOne([1, 2, 3])); // [1, 2, 4]
+  console.log(`plusOne([4, 3, 2, 1]):`, plusOne([4, 3, 2, 1])); // [4, 3, 2, 2]
+  console.log(`plusOne([0]):`, plusOne([0])); // [1]
+}
