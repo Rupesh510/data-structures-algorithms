bc356a95e60c0e95438b48173e8b044fdf9dc823
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-18 14:14:49 +0530

    Create exercise "Best Time to Buy and Sell Stock"

---
+++ 101-leetcode/038/question.js
@@ -0,0 +1,11 @@
+/**
+ * Best Time to Buy and Sell Stock
+ * https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+ */
+
+{
+  function maxProfit(prices) {}
+
+  console.log(maxProfit([7, 1, 5, 3, 6, 4])); // 5
+  console.log(maxProfit([7, 6, 4, 3, 1])); // 0
+}
