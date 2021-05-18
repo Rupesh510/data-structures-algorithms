d9fa33c56d511ab103f0d87c943c321586c952f5
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-19 00:19:17 +0530

    Working Solution for "Plus One" exercise

--- 101-leetcode/041/solution.js
+++ 101-leetcode/041/solution.js
@@ -1,10 +1,23 @@
 /**
  * Plus One
  * https://leetcode.com/problems/plus-one/
+ * Solution Ref: https://youtu.be/Llk4N7ERILg
  */
 
 {
-  function plusOne(digits) {}
+  function plusOne(digits) {
+    for (let i = digits.length - 1; i >= 0; i -= 1) {
+      if (digits[i] !== 9) {
+        digits[i] += 1;
+        return digits;
+      } else {
+        digits[i] = 0;
+      }
+    }
+
+    digits.unshift(1);
+    return digits;
+  }
 
   console.log(`plusOne([1, 2, 3]):`, plusOne([1, 2, 3])); // [1, 2, 4]
   console.log(`plusOne([4, 3, 2, 1]):`, plusOne([4, 3, 2, 1])); // [4, 3, 2, 2]
