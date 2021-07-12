9f71a6a2db2e3a21dab40e2ffb0239760c6dfceb
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-07-12 16:59:12 +0530

    remove unused code

--- 001-big-o/005.js
+++ 001-big-o/005.js
@@ -3,19 +3,6 @@
  */
 
 {
-  const nemo = ["nemo"];
-  const everyone = [
-    "dory",
-    "bruce",
-    "marlin",
-    "nemo",
-    "gill",
-    "bloat",
-    "nigel",
-    "squirt",
-    "darla",
-    "hank",
-  ];
   const large = new Array(1000).fill("nemo");
 
   function findNemo(array) {
