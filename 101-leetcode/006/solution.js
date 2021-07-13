fc11ecea38dd42f1fb83bf95d1a91894cc73dba9
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-07-13 18:09:06 +0530

    chore: update leetcode solution

--- 101-leetcode/006/solution.js
+++ 101-leetcode/006/solution.js
@@ -16,16 +16,16 @@
   function rotate(nums, k) {
     k = k % nums.length;
     reverse(nums, 0, nums.length - 1);
-    reverse(nums, k, nums.length - 1);
     reverse(nums, 0, k - 1);
+    reverse(nums, k, nums.length - 1);
     return nums;
   }
 
-  function reverse(nums, start, end) {
-    while (start < end) {
-      [nums[start], nums[end]] = [nums[end], nums[start]];
-      start += 1;
-      end -= 1;
+  function reverse(nums, p1, p2) {
+    while (p1 < p2) {
+      [nums[p1], nums[p2]] = [nums[p2], nums[p1]];
+      p1 += 1;
+      p2 -= 1;
     }
   }
 
