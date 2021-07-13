1a81d2a4a0c0828b45f0015020094f6b9447fea0
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-07-13 19:40:18 +0530

    fix bug

--- 101-leetcode/011/carbon/002.js
+++ 101-leetcode/011/carbon/002.js
@@ -4,8 +4,8 @@
 
 function isPalindrome(s) {
   s = s.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
-  let left = 0;
-  right = s.length - 1;
+  let left = 0,
+    right = s.length - 1;
   while (left < right) {
     if (s[left] !== s[right]) return false;
     left += 1;
