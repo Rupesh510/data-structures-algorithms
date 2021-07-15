d80e2591992ccf6480f8923e021b0a857518558c
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-07-15 20:19:32 +0530

    Self Test: Optimal - Backspace String Compare

--- 103-sublime/009/solution.js
+++ 103-sublime/009/solution.js
@@ -82,7 +82,39 @@ console.log("\n================================\n");
    * Space complexity: O(1)
    */
 
-  function backspaceCompare(S, T) {}
+  function backspaceCompare(S, T) {
+    let pS = S.length - 1,
+      pT = T.length - 1;
+    while (pS >= 0 && pT >= 0) {
+      if (S[pS] === "#" || T[pT] === "#") {
+        if (S[pS] === "#") {
+          pS = movePointer(S, pS);
+        } else {
+          pT = movePointer(T, pT);
+        }
+      } else {
+        if (S[pS] !== T[pT]) {
+          return false;
+        } else {
+          pS -= 1;
+          pT -= 1;
+        }
+      }
+    }
+    return true;
+  }
+
+  function movePointer(P, p) {
+    let backCount = 2;
+    while (backCount > 0) {
+      p -= 1;
+      backCount -= 1;
+      if (P[p] === "#") {
+        backCount += 2;
+      }
+    }
+    return p;
+  }
 
   console.log(
     `backspaceCompare("ab#c", "ad#c"):`,
