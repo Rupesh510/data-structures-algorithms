ba793f91135b23c8b8650e142dc9ac18808d1c9d
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-07-16 14:28:22 +0530

    Self Test: Optimal - Longest Substring Without Repeating Characters

--- 103-sublime/010/solution.js
+++ 103-sublime/010/solution.js
@@ -69,7 +69,22 @@ console.log("\n================================\n");
    * Space complexity: O(n)
    */
 
-  function lengthOfLongestSubstring(s) {}
+  function lengthOfLongestSubstring(s) {
+    if (s.length <= 1) return s.length;
+    const seen = {};
+    let left = 0,
+      longest = 0;
+    for (let right = 0; right < s.length; right += 1) {
+      const currentChar = s[right];
+      const previouslySeenChar = seen[currentChar];
+      if (previouslySeenChar >= left) {
+        left = previouslySeenChar + 1;
+      }
+      seen[currentChar] = right;
+      longest = Math.max(longest, right - left + 1)
+    }
+    return longest;
+  }
 
   console.log(
     `lengthOfLongestSubstring("abccabb"):`,
@@ -109,7 +124,22 @@ console.log("\n================================\n");
    * Space complexity: O(n)
    */
 
-  function lengthOfLongestSubstring(s) {}
+  function lengthOfLongestSubstring(s) {
+    if (s.length <= 1) return s.length;
+    const seen = new Map();
+    let left = 0,
+      longest = 0;
+    for (let right = 0; right < s.length; right += 1) {
+      const currentChar = s[right];
+      const previouslySeenChar = seen.get(currentChar);
+      if (previouslySeenChar >= left) {
+        left = previouslySeenChar + 1;
+      }
+      seen.set(currentChar, right);
+      longest = Math.max(longest, right - left + 1)
+    }
+    return longest;
+  }
 
   console.log(
     `lengthOfLongestSubstring("abccabb"):`,
